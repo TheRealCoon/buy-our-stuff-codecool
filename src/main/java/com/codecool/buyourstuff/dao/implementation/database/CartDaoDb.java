104b7c6856b96267e6306fe8d099370a263c7ed9
T.Sanyi <s.truczkai@gmail.com>
2022-11-22 17:48:58 +0100

    create packages for DAOs

---
+++ src/main/java/com/codecool/buyourstuff/dao/implementation/database/CartDaoDb.java
@@ -0,0 +1,4 @@
+package com.codecool.buyourstuff.dao.implementation.database;
+
+public class CartDaoDb {
+}
---
+++ src/main/java/com/codecool/buyourstuff/dao/implementation/file/CartDaoFile.java
@@ -0,0 +1,4 @@
+package com.codecool.buyourstuff.dao.implementation.file;
+
+public class CartDaoFile {
+}
