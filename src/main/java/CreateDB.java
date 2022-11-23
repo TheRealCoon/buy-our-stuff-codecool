892e92494f9ed50ba5167d82cc0b91c5f749d8cb
T.Sanyi <s.truczkai@gmail.com>
2022-11-23 01:20:03 +0100

    fix a bug in createDB,it can create a database now

--- src/main/java/CreateDB.java
+++ src/main/java/CreateDB.java
@@ -3,12 +3,13 @@ import com.codecool.buyourstuff.util.BaseData;
 import java.sql.*;
 
 public class CreateDB {
-    //Todo you need to set 'USER' and 'PASSWORD' in environment variables
+    //TODO you need to set 'USER' and 'PASSWORD' in environment variables
+
     private static final String DEFAULT_DBNAME = "postgres";
     private static final String LOCALHOST_5432 = "jdbc:postgresql://localhost:5432/";
-    private static final String USER = "USER";
-    private static final String PASSWORD = "PASSWORD";
-    private static final String DB_NAME = "BOS_webshop";
+    private static final String USER = System.getenv("USER");
+    private static final String PASSWORD = System.getenv("PASSWORD");
+    private static final String DB_NAME = "bos_webshop";
     private Connection connection;
 
     public CreateDB(Connection connection) {
@@ -19,7 +20,7 @@ public class CreateDB {
         try (Connection con = DriverManager.getConnection(LOCALHOST_5432 + DEFAULT_DBNAME, USER, PASSWORD)) {
             System.out.println("Creating database...");
             CreateDB createDB = new CreateDB(con);
-            createDB.createDataBase(DB_NAME);
+            createDB.createDataBase();
             System.out.println("Database created");
         } catch (SQLException e){
             e.printStackTrace();
@@ -31,14 +32,9 @@ public class CreateDB {
         }
     }
 
-    private void createDataBase(String name) throws SQLException {
-        String SqlQuery = "CREATE DATABASE ?;";
-        PreparedStatement ps = connection.prepareStatement(SqlQuery);
-        ps.setString(1, name);
-        ResultSet rs = ps.executeQuery();
-    }
-
-    public void setConnection(Connection connection) {
-        this.connection = connection;
+    private void createDataBase() throws SQLException {
+        String SqlQuery = "CREATE DATABASE " + DB_NAME;
+        Statement statement = connection.createStatement();
+        statement.execute(SqlQuery);
     }
 }
