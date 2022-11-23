ead2d0d94bc0b3d64872a8bf04b6ba95f6b83c07
T.Sanyi <s.truczkai@gmail.com>
2022-11-23 19:59:19 +0100

    add constructor to CartDaoDb

--- src/main/java/com/codecool/buyourstuff/dao/implementation/database/CartDaoDb.java
+++ src/main/java/com/codecool/buyourstuff/dao/implementation/database/CartDaoDb.java
@@ -3,9 +3,14 @@ package com.codecool.buyourstuff.dao.implementation.database;
 import com.codecool.buyourstuff.dao.CartDao;
 import com.codecool.buyourstuff.model.Cart;
 
+import java.sql.Connection;
 import java.util.List;
 
 public class CartDaoDb implements CartDao {
+    private final Connection connection;
+    public CartDaoDb(Connection connection){
+        this.connection = connection;
+    }
     @Override
     public void add(Cart cart) {
 
