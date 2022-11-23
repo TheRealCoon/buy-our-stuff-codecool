70525a461e1baeec492d0ce79828ab64cb6a3438
T.Sanyi <s.truczkai@gmail.com>
2022-11-23 20:45:02 +0100

    implement find in CartDaoDb

--- src/main/java/com/codecool/buyourstuff/dao/implementation/database/CartDaoDb.java
+++ src/main/java/com/codecool/buyourstuff/dao/implementation/database/CartDaoDb.java
@@ -2,6 +2,7 @@ package com.codecool.buyourstuff.dao.implementation.database;
 
 import com.codecool.buyourstuff.dao.CartDao;
 import com.codecool.buyourstuff.model.Cart;
+import com.codecool.buyourstuff.model.exception.DataNotFoundException;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -9,12 +10,14 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.List;
 
+import static com.codecool.buyourstuff.dao.implementation.database.DbLogin.*;
+
 public class CartDaoDb implements CartDao {
 
     @Override
     public void add(Cart cart) {
         String SqlQuery = "INSERT INTO cart(currency) VALUES(?);";
-        try (Connection connection = DriverManager.getConnection(LOCALHOST_5432 + DEFAULT_DBNAME, USER, PASSWORD)) {
+        try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD)) {
             PreparedStatement ps = connection.prepareStatement(SqlQuery);
             ps.setString(1, cart.getCurrency().toString());
             ps.execute();
@@ -25,7 +28,20 @@ public class CartDaoDb implements CartDao {
 
     @Override
     public Cart find(int id) {
-        return null;
+        String SqlQuery = "SELECT currency FROM cart WHERE id = ?;";
+        Cart cart = null;
+        try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD)) {
+            PreparedStatement ps = connection.prepareStatement(SqlQuery);
+            ps.setInt(1, id);
+             String currency = ps.executeQuery().getString(1);
+            if (currency!=null && !currency.isEmpty()){
+                cart = new Cart(currency);
+                cart.setId(id);
+            }else throw new DataNotFoundException("No such cart");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return cart;
     }
 
     @Override
