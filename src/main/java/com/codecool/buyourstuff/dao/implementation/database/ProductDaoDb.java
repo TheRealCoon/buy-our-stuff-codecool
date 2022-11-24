4253cbfc29d51749636110052f97513d3a873038
T.Sanyi <s.truczkai@gmail.com>
2022-11-24 22:30:14 +0100

    add ProductDaoDb skeleton

---
+++ src/main/java/com/codecool/buyourstuff/dao/implementation/database/ProductDaoDb.java
@@ -0,0 +1,45 @@
+package com.codecool.buyourstuff.dao.implementation.database;
+
+import com.codecool.buyourstuff.dao.ProductDao;
+import com.codecool.buyourstuff.model.Product;
+import com.codecool.buyourstuff.model.ProductCategory;
+import com.codecool.buyourstuff.model.Supplier;
+
+import java.util.List;
+
+public class ProductDaoDb implements ProductDao {
+    @Override
+    public void add(Product product) {
+
+    }
+
+    @Override
+    public Product find(int id) {
+        return null;
+    }
+
+    @Override
+    public void remove(int id) {
+
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public List<Product> getAll() {
+        return null;
+    }
+
+    @Override
+    public List<Product> getBy(Supplier supplier) {
+        return null;
+    }
+
+    @Override
+    public List<Product> getBy(ProductCategory productCategory) {
+        return null;
+    }
+}
