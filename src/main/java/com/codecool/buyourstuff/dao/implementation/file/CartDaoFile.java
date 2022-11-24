ff8cb2c5f7deda2ab48a6a6c88c69c0131890094
T.Sanyi <s.truczkai@gmail.com>
2022-11-24 19:03:05 +0100

    implement add in CartDaoFile

--- src/main/java/com/codecool/buyourstuff/dao/implementation/file/CartDaoFile.java
+++ src/main/java/com/codecool/buyourstuff/dao/implementation/file/CartDaoFile.java
@@ -3,12 +3,24 @@ package com.codecool.buyourstuff.dao.implementation.file;
 import com.codecool.buyourstuff.dao.CartDao;
 import com.codecool.buyourstuff.model.Cart;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
+import java.util.StringJoiner;
 
 public class CartDaoFile implements CartDao {
+    private static final String CART_FILE = "data/cart.csv";
+    private static final char DATA_SEPARATOR = ';';
+
     @Override
     public void add(Cart cart) {
-
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter(CART_FILE, true))) {
+            String line = String.valueOf(cart.getId()) + DATA_SEPARATOR + cart.getCurrency();
+            writer.append(line);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
     }
 
     @Override
