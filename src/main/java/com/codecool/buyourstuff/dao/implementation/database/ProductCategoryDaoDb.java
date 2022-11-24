f72e611d3c0ca9cae116a60ad223d0ddab9c724a
T.Sanyi <s.truczkai@gmail.com>
2022-11-24 16:43:00 +0100

    implement find in ProductCategoryDaoDb

--- src/main/java/com/codecool/buyourstuff/dao/implementation/database/ProductCategoryDaoDb.java
+++ src/main/java/com/codecool/buyourstuff/dao/implementation/database/ProductCategoryDaoDb.java
@@ -2,12 +2,11 @@ package com.codecool.buyourstuff.dao.implementation.database;
 
 import com.codecool.buyourstuff.dao.CartDao;
 import com.codecool.buyourstuff.dao.ProductCategoryDao;
+import com.codecool.buyourstuff.model.Cart;
 import com.codecool.buyourstuff.model.ProductCategory;
+import com.codecool.buyourstuff.model.exception.DataNotFoundException;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.List;
 
 import static com.codecool.buyourstuff.dao.implementation.database.DbLogin.*;
@@ -15,7 +14,7 @@ import static com.codecool.buyourstuff.dao.implementation.database.DbLogin.*;
 public class ProductCategoryDaoDb implements ProductCategoryDao {
     @Override
     public void add(ProductCategory category) {
-        String SqlQuery = "INSERT INTO product_category(\"name\", description, department) VALUES(?, ?, ?);";
+        String SqlQuery = "INSERT INTO product_categories(\"name\", description, department) VALUES(?, ?, ?);";
         try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD)) {
             PreparedStatement ps = connection.prepareStatement(SqlQuery);
             ps.setString(1, category.getName());
@@ -29,7 +28,23 @@ public class ProductCategoryDaoDb implements ProductCategoryDao {
 
     @Override
     public ProductCategory find(int id) {
-        return null;
+        String SqlQuery = "SELECT \"name\", description, department FROM product_categories WHERE id = ?;";
+        ProductCategory pc = null;
+        try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD)) {
+            PreparedStatement ps = connection.prepareStatement(SqlQuery);
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+            String name = rs.getString(1);
+            String description = rs.getString(2);
+            String department = rs.getString(3);
+            if (name != null) {
+                pc = new ProductCategory(name, description, department);
+                pc.setId(id);
+            } else throw new DataNotFoundException("No such product category");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return pc;
     }
 
     @Override
