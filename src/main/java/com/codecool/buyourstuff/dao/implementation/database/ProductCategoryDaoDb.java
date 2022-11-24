d20a7750d7eefe013aee736628effb23bafd705f
T.Sanyi <s.truczkai@gmail.com>
2022-11-24 16:36:55 +0100

    implement add in ProductCategoryDaoDb

--- src/main/java/com/codecool/buyourstuff/dao/implementation/database/ProductCategoryDaoDb.java
+++ src/main/java/com/codecool/buyourstuff/dao/implementation/database/ProductCategoryDaoDb.java
@@ -4,12 +4,27 @@ import com.codecool.buyourstuff.dao.CartDao;
 import com.codecool.buyourstuff.dao.ProductCategoryDao;
 import com.codecool.buyourstuff.model.ProductCategory;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
+
+import static com.codecool.buyourstuff.dao.implementation.database.DbLogin.*;
 
 public class ProductCategoryDaoDb implements ProductCategoryDao {
     @Override
     public void add(ProductCategory category) {
-
+        String SqlQuery = "INSERT INTO product_category(\"name\", description, department) VALUES(?, ?, ?);";
+        try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD)) {
+            PreparedStatement ps = connection.prepareStatement(SqlQuery);
+            ps.setString(1, category.getName());
+            ps.setString(2, category.getDescription());
+            ps.setString(3, category.getDepartment());
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
