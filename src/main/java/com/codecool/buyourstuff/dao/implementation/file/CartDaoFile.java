73e40f53ce81eeb77d7ea1f789d085555ce086fc
T.Sanyi <s.truczkai@gmail.com>
2022-11-24 19:24:06 +0100

    implement find in CartDaoFile

--- src/main/java/com/codecool/buyourstuff/dao/implementation/file/CartDaoFile.java
+++ src/main/java/com/codecool/buyourstuff/dao/implementation/file/CartDaoFile.java
@@ -2,30 +2,50 @@ package com.codecool.buyourstuff.dao.implementation.file;
 
 import com.codecool.buyourstuff.dao.CartDao;
 import com.codecool.buyourstuff.model.Cart;
+import com.codecool.buyourstuff.model.exception.DataNotFoundException;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.StringJoiner;
 
 public class CartDaoFile implements CartDao {
     private static final String CART_FILE = "data/cart.csv";
-    private static final char DATA_SEPARATOR = ';';
+    private static final String DATA_SEPARATOR = ";";
 
     @Override
     public void add(Cart cart) {
-        try(BufferedWriter writer = new BufferedWriter(new FileWriter(CART_FILE, true))) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(CART_FILE, true))) {
             String line = String.valueOf(cart.getId()) + DATA_SEPARATOR + cart.getCurrency();
             writer.append(line);
-        }catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     @Override
     public Cart find(int id) {
-        return null;
+        Cart cart = null;
+        String line;
+        try (BufferedReader reader = new BufferedReader(new FileReader(CART_FILE))) {
+            while ((line = reader.readLine()) != null && cart == null) {
+                String[] values = line.split(DATA_SEPARATOR);
+                int resultId = Integer.parseInt(values[0]);
+                String resultCurrency = values[1];
+                if (resultId == id) {
+                    cart = new Cart(resultCurrency);
+                    cart.setId(resultId);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid ID in file!");
+        }
+        if (cart == null) {
+            throw new DataNotFoundException("Cart with id = " + id + "not found!");
+        }
+        return cart;
     }
 
     @Override
