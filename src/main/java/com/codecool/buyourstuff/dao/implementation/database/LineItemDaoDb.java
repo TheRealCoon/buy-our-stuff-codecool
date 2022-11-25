4bd72cadbc5c0f45611c4576d881207a0ac2e70c
urbansoma4 <75248194+urbansoma4@users.noreply.github.com>
2022-11-25 03:31:53 +0100

    line item halfway inside

---
+++ src/main/java/com/codecool/buyourstuff/dao/implementation/database/LineItemDaoDb.java
@@ -0,0 +1,110 @@
+package com.codecool.buyourstuff.dao.implementation.database;
+
+import com.codecool.buyourstuff.dao.LineItemDao;
+import com.codecool.buyourstuff.dao.ProductDao;
+import com.codecool.buyourstuff.model.Cart;
+import com.codecool.buyourstuff.model.LineItem;
+import com.codecool.buyourstuff.model.Product;
+import com.codecool.buyourstuff.model.ProductCategory;
+import com.codecool.buyourstuff.model.exception.DataNotFoundException;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.codecool.buyourstuff.dao.implementation.database.DbLogin.*;
+
+public class LineItemDaoDb implements LineItemDao {
+    @Override
+    public void add(LineItem lineItem) {
+        String sql = "INSERT INTO line_items(product_id, cart_id, quantity) VALUES(?, ?, ?);";
+        try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD)) {
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setInt(1, lineItem.getProduct().getId());
+            ps.setInt(2, lineItem.getCartId());
+            ps.setInt(3, lineItem.getQuantity());
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void remove(LineItem lineItem) {
+        String sql = "DELETE FROM line_items WHERE id = ?;";
+        try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD)) {
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(1, lineItem.getId());
+            if (!preparedStatement.execute()) throw new DataNotFoundException("No line item found!");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void clear() {
+        String sql = "DELETE FROM line_items;";
+        try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD)) {
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void update(LineItem lineItem, int quantity) {
+        String sql = "UPDATE line_items SET quantity ";
+
+    }
+
+    @Override
+    public LineItem find(int id) {
+        String SqlQuery = "SELECT product_id, cart_id, quantity FROM line_items WHERE id = ?;";
+        LineItem lineItem = null;
+        try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD)) {
+            PreparedStatement ps = connection.prepareStatement(SqlQuery);
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+            int product = rs.getInt(1);
+            int cartId = rs.getInt(2);
+            int quantity = rs.getInt(3);
+            if (product != null) {
+                lineItem = new LineItem(productId, cartId, quantity);
+                lineItem.setId(id);
+            } else throw new DataNotFoundException("No such line item");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return lineItem;
+    }
+
+    @Override
+    public List<LineItem> getBy(Cart cart) {
+        /*ProductDao productDataStore = DaoController.getProductDao();
+        int cartId = cart.getId();
+        String query = "SELECT * FROM line_item WHERE cart_id = " + cartId + " ORDER BY id;";
+        List<LineItem> result = new ArrayList<>();
+
+        try {
+            @Cleanup Connection conn = getConnection();
+            @Cleanup Statement stmt = conn.createStatement();
+            @Cleanup ResultSet rs = stmt.executeQuery(query);
+
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                int productId = rs.getInt("product_id");
+                int quantity = rs.getInt("quantity");
+                Product product = productDataStore.find(productId);
+                LineItem lineItem = new LineItem(product, cart.getId(), quantity);
+                lineItem.setId(id);
+                result.add(lineItem);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+*/
+        return null; //result
+    }
+}
---
+++ src/main/java/com/codecool/buyourstuff/dao/implementation/file/LineItemDaoFile.java
@@ -0,0 +1,113 @@
+package com.codecool.buyourstuff.dao.implementation.file;
+
+import com.codecool.buyourstuff.dao.CartDao;
+import com.codecool.buyourstuff.dao.DataManager;
+import com.codecool.buyourstuff.dao.LineItemDao;
+import com.codecool.buyourstuff.model.*;
+import com.codecool.buyourstuff.model.exception.DataNotFoundException;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+import java.util.Scanner;
+
+import static java.net.NetworkInterface.getAll;
+
+public class LineItemDaoFile implements LineItemDao {
+    private final File LINEITEM_FILE = new File("data/lineitems.csv");
+    private int nextLineItemId;
+    private static final String DATA_SEPARATOR = ";";
+    private static int highestID;
+
+    public LineItemDaoFile() {
+        if (LINEITEM_FILE.exists()) {
+            try (Scanner scanner = new Scanner(LINEITEM_FILE)) {
+                scanner.useDelimiter(",");
+                while (scanner.hasNextLine()) {
+                    scanner.nextLine();
+                }
+                scanner.findInLine("id=");
+                int lastUserId = scanner.nextInt();
+                nextLineItemId = lastUserId + 1;
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        else nextLineItemId = 1;
+    }
+
+    @Override
+    public void add(LineItem lineItem) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(LINEITEM_FILE, true))) {
+            int id = lineItem.getId();
+            if (id == 0) {
+                id = ++highestID;
+                lineItem.setId(id);
+            } else if (id > highestID) {
+                highestID = id;
+            }
+            String line = id + DATA_SEPARATOR + lineItem.getProduct() + DATA_SEPARATOR + lineItem.getCartId() + DATA_SEPARATOR + lineItem.getQuantity();
+            writer.append(line).append(System.lineSeparator());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void remove(LineItem lineItem) {
+        List<LineItem> lineItems = getAll();
+        lineItems.removeIf(c -> c.getId() == id);
+        overWriteListOfCartsInFile(lineItems);
+    }
+
+    @Override
+    public void clear() {
+        try {
+            new FileWriter(LINEITEM_FILE, false).close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        nextLineItemId = 1;
+    }
+
+    @Override
+    public void update(LineItem lineItem, int quantity) {
+
+    }
+
+    @Override
+    public LineItem find(int id) {
+        try (Scanner scanner = new Scanner(LINEITEM_FILE)) {
+            while (scanner.hasNextLine()) {
+                LineItem lineItem = new LineItem(
+                        String.valueOf(scanner.nextInt()),
+                        scanner.nextInt(),
+                        scanner.nextInt());
+                if (id == lineItem.getId())
+                    return lineItem;
+            }
+            throw new DataNotFoundException("No such user. Name or password may be incorrect.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public List<LineItem> getBy(Cart cart) {
+        return null;
+    }
+
+    private void overWriteListOfCartsInFile(List<LineItem> lineItems) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(LINEITEM_FILE, false))) {
+            for (LineItem lineItem : lineItems) {
+                String line = lineItem.getId() + DATA_SEPARATOR + lineItem.getProduct() + DATA_SEPARATOR + lineItem.getCartId() + DATA_SEPARATOR + lineItem.getQuantity()
+                writer.append(line).append(System.lineSeparator());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
